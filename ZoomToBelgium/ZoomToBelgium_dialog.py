cdb2a2951848103aaf53ab21b48c494b21e6c733
Michel Stuyts <michel.stuyts@gmail.com>
2017-09-21 10:28:46 +0200

    first working version

--- ZoomToBelgium/ZoomToBelgium_dialog.py
+++ ZoomToBelgium/ZoomToBelgium_dialog.py
@@ -22,20 +22,28 @@
 """
 
 import os
-
-from PyQt4 import QtGui, uic
+try:
+    from qgis.PyQt.QtCore import *
+except ImportError:
+    from PyQt4.QtCore import *
+try:
+    from qgis.PyQt import uic
+except ImportError:
+    from PyQt4 import uic
+try:
+    from qgis.PyQt.QtGui import QIcon
+    from qgis.PyQt.QtWidgets import QDialog
+except ImportError:
+    from PyQt4.QtGui import QIcon, QDialog
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'ZoomToBelgium_dialog_base.ui'))
 
 
-class ZoomToBelgiumDialog(QtGui.QDialog, FORM_CLASS):
+class ZoomToBelgiumDialog(QDialog, FORM_CLASS):
     def __init__(self, parent=None):
-        """Constructor."""
         super(ZoomToBelgiumDialog, self).__init__(parent)
-        # Set up the user interface from Designer.
-        # After setupUI you can access any designer object by doing
-        # self.<objectname>, and you can use autoconnect slots - see
-        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
-        # #widgets-and-dialogs-with-auto-connect
+        self.resize(QSize(400, 230).expandedTo(self.minimumSizeHint()))
+        self.setWindowIcon(QIcon(":/plugins/ZoomToBelgium/icon.png"))
+        self.setWindowFlags( self.windowFlags() & ~Qt.WindowContextHelpButtonHint |  Qt.CustomizeWindowHint  | Qt.WindowTitleHint  )
         self.setupUi(self)
